98bdacda725033a863675436b4f6e79013f8a0fd
Thai Son Nguyen <thaison.nguyen@ncs.com.sg>
2019-06-26 00:04:13 +0800

    draft version

---
+++ wedding/Woh5RBu_wZ0.js
@@ -0,0 +1,3 @@
+if (self.CavalryLogger) { CavalryLogger.start_js(["5hhii"]); }
+
+__d("SchedulerFeatureFlags",[],(function(a,b,c,d,e,f){a={enableSchedulerDebugging:!0,enableIsInputPending:!0};e.exports=a}),null);
---
+++ wedding/main.js
@@ -0,0 +1,135 @@
+$(document).ready(function() {
+   
+    /* ======= Scrollspy ======= */
+    $('body').scrollspy({ target: '#header', offset: 100});
+    
+    /* ======= ScrollTo ======= */
+    $('a.scrollto').on('click', function(e){
+        
+        //store hash
+        var target = this.hash;
+                
+        e.preventDefault();
+        
+		$('body').scrollTo(target, 800, {offset: -55, 'axis':'y'});
+        //Collapse mobile menu after clicking
+		if ($('.navbar-collapse').hasClass('show')){
+			$('.navbar-collapse').removeClass('show');
+		}
+		
+	});        
+    
+    /* ======= Countdown ========= */
+	// set the date we're counting down to
+    var target_date = new Date("July 16, 2020").getTime();
+     
+    // variables for time units
+    var days, hours, minutes, seconds;
+     
+    // get tag element
+    var countdown =  document.getElementById("countdown-box");
+    var days_span = document.createElement("SPAN");
+    days_span.className = 'days';
+    countdown.appendChild(days_span);
+    var hours_span = document.createElement("SPAN");
+    hours_span.className = 'hours';
+    countdown.appendChild(hours_span);
+    var minutes_span = document.createElement("SPAN");
+    minutes_span.className = 'minutes';
+    countdown.appendChild(minutes_span);
+    var secs_span = document.createElement("SPAN");
+    secs_span.className = 'secs';
+    countdown.appendChild(secs_span);
+     
+    // update the tag with id "countdown" every 1 second
+    setInterval(function () {
+     
+        // find the amount of "seconds" between now and target
+        var current_date = new Date().getTime();
+        var seconds_left = (target_date - current_date) / 1000;
+     
+        // do some time calculations
+        days = parseInt(seconds_left / 86400);
+        seconds_left = seconds_left % 86400;
+         
+        hours = parseInt(seconds_left / 3600);
+        seconds_left = seconds_left % 3600;
+         
+        minutes = parseInt(seconds_left / 60);
+        seconds = parseInt(seconds_left % 60);
+         
+        // format countdown string + set tag value.
+        days_span.innerHTML = '<span class="number">' + days + '</span>' + '<span class="unit script">Days</span>';
+        hours_span.innerHTML = '<span class="number">' + hours + '</span>' + '<span class="unit script">Hrs</span>';
+        minutes_span.innerHTML = '<span class="number">' + minutes + '</span>' + '<span class="unit script">Mins</span>';
+        secs_span.innerHTML = '<span class="number">' + seconds + '</span>' + '<span class="unit script">Secs</span>';
+        
+      
+        //countdown.innerHTML = days + "d, " + hours + "h, "
+       // + minutes + "m, " + seconds + "s";  
+     
+    }, 1000);
+     
+
+    
+
+    
+    /* ===== Packery ===== */
+    //Ref: http://packery.metafizzy.co/
+    //Ref: http://imagesloaded.desandro.com/
+
+    var $container = $('#photos-wrapper');
+    
+    // init
+    $container.imagesLoaded(function () {
+        $container.packery({
+            itemSelector: '.item',
+            percentPosition: true
+        });
+    });
+    
+    
+    /* ======= RSVP Form (Dependent form field) ============ */
+    $('#cguests').on("change", function(){
+        
+        if ($(this).val() == "") {
+            $('.guestinfo-group').slideUp(); //hide
+            console.log('not selected');
+        } else if ($(this).val() == 'No Guests' ) {
+            $('.guestinfo-group').slideUp(); //hide
+            console.log('No guests');
+            $('#cguestinfo').val('No Guests'); //Pass data to the field so mailer.php can send the form.
+            
+        } else {
+            $('.guestinfo-group').slideDown(); //show
+            $('#cguestinfo').val(''); //Clear data
+            console.log('Has guests');
+        }
+
+       
+    });
+    
+    /* ======= jQuery form validator ======= */ 
+    /* Ref: http://jqueryvalidation.org/documentation/ */   
+    $(".rsvp-form").validate({
+		messages: {
+		    name: {
+    			required: 'Please enter your full name' //You can customise this message
+			},
+			email: {
+				required: 'Please enter your email' //You can customise this message
+			},
+			events: {
+				required: 'Are you attending?' //You can customise this message
+			},
+			guests: {
+				required: 'How many guests?' //You can customise this message
+			},
+			guestinfo: {
+				required: 'Please provide name(s)' //You can customise this message
+			},
+		}
+	});
+  
+
+});
